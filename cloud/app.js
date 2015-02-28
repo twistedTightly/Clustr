4008e6d176ee7050e942db316d6d44d7b3d74445
twistedTightly <mrauh1@nd.edu>
2015-02-28 16:21:20 -0600

    Log in and sign up working

--- cloud/app.js
+++ cloud/app.js
@@ -8,31 +8,35 @@ app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
 
-// This is an example of hooking up a request handler with a specific request
-// path and HTTP verb using the Express routing API.
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
-});
 
+// Called when sign up form submitted from main page
 app.post('/home', function(req, res) {
 	res.render('home', { name: req.body.firstname });
 });
 
+// Called when log in form submitted from main page
 app.get('/home', function(req, res) {
-	res.render('home', { name: req.query.email });
+	// Create a Parse query to get the user's name
+	var query = new Parse.Query(Parse.User);
+	query.equalTo('username', req.query.email);
+
+	// Execute the query
+	var firstname = req.query.email;
+	query.find({
+		success: function(results) {
+			console.error('results', results);
+			if (results) {
+				firstname = results[0].attributes.firstname;
+				console.error('first name', firstname);
+				res.render('home', { name: firstname });
+			}
+		},
+		error: function(error) {
+			console.error('error', error.message);
+			res.render('home', { name: firstname });
+		}
+	});
 });
-
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
-
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
 
 // Attach the Express app to Cloud Code.
 app.listen();
--- public/js/main.js
+++ public/js/main.js
@@ -14,9 +14,20 @@ Parse.initialize("APP-KEY", "JAVASCRIPT-ID");
 // Log in
 $('form#log-in').on('submit', function(){
 	// Get information from inputs
-	var email = $('form#sign-up #user-email').val(),
-		password = $('form#sign-up #password').val();
+	var email = $('form#log-in #user-email').val(),
+		password = $('form#log-in #password').val();
 
+	Parse.User.logIn(email, password, {
+		success: function(user) {
+			document.logIn.submit();
+		},
+		error: function(user, error) {
+			// The login failed. Check error to see why.
+			alert('Uh oh! ' + error.message + ' (' + error.code + ')');
+			document.logIn.submit();
+		}
+	});
+	return false;
 });
 
 // Sign up
@@ -27,5 +38,23 @@ $('form#sign-up').on('submit', function(){
 		major = $('form#sign-up #major').val() || '',
 		email = $('form#sign-up #user-email').val(),
 		password = $('form#sign-up #password').val();
-
+	
+	var user = new Parse.User();
+	user.set("username", email);
+	user.set("firstname", firstName);
+	user.set("lastname", lastName);
+	user.set("major", major);
+	user.set("password", password);
+	user.set("role", "student");
+	  
+	user.signUp(null, {
+	  success: function(user) {
+	  	document.signIn.submit();
+	  },
+	  error: function(user, error) {
+	    // Show the error message somewhere and let the user try again.
+	    alert("Error: " + error.code + " " + error.message);
+	  }
+	});
+	return false;
 });
