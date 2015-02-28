0997af4791e5a7610e051cdbc274e5e8fb3dccb3
twistedTightly <mrauh1@nd.edu>
2015-02-28 11:49:20 -0600

    Sass and Foundation added

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+/* main.js
+ *
+ * Handles the log in and sign up from the landing page
+ *
+ */
+
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
